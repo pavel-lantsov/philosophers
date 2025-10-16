cc038199df6038343b7a38c3519226ca04f063a4
plantsov <plantsov@student.42berlin.de>
2025-10-16 17:45:38 +0200

    fix phillo

--- srcs/reaper.c
+++ srcs/reaper.c
@@ -16,7 +16,8 @@ void	stop(t_data *data)
 	data->stop_flag = 1;
 	pthread_mutex_unlock(&data->death);
 }
-static int check_phills(t_phil *phils, t_data *data)
+
+static int	check_phills(t_phil *phils, t_data *data)
 {
 	long	cur_time;
 	int		i;
@@ -26,24 +27,25 @@ static int check_phills(t_phil *phils, t_data *data)
 	cur_time = get_timestamp();
 	full = 0;
 	while (i < data->num_phil && !is_dead(data))
+	{
+		if (cur_time - phils[i].lst_meal_time > data->time_die)
 		{
-			if (cur_time - phils[i].lst_meal_time > data->time_die)
-			{
-				safe_print(&phils[i], "died");
-				stop(data);
-				return (1);
-			}
-			if (data->must_eaten != -1 && phils[i].meals >= data->must_eaten)
-				full++;
-			i++;
-		}
-		if (data->must_eaten > 0 && full == data->num_phil)
-		{
+			safe_print(&phils[i], "died");
 			stop(data);
 			return (1);
 		}
-	return(0);
+		if (data->must_eaten != -1 && phils[i].meals >= data->must_eaten)
+			full++;
+		i++;
+	}
+	if (data->must_eaten > 0 && full == data->num_phil)
+	{
+		stop(data);
+		return (1);
+	}
+	return (0);
 }
+
 void	*death_monitor(void *arg)
 {
 	t_phil	*phils;
@@ -53,10 +55,9 @@ void	*death_monitor(void *arg)
 	data = phils[0].data;
 	while (!is_dead(data))
 	{
-		if(check_phills(phils, data))
-			break;
+		if (check_phills(phils, data))
+			break ;
 		ft_usleep(1);
 	}
 	return (NULL);
 }
-
